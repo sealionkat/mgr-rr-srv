37abbbcad092e9cecb1a22444dc6aa4715e7b6d5
sealionkat <sealionkat@gmail.com>
2017-04-23 00:14:21 +0200

    Added intersection algorithms and tests.

--- utils/geo.test.js
+++ utils/geo.test.js
@@ -1,10 +1,23 @@
+/* global describe, it, require */
 const expect = require('chai').expect;
 const geo = require('./geo');
 
 describe('geo utilis', () => {
   describe('intersectionDetection()', () => {
+    /*it('should throw error', () => {
+      try {
+        geo.intersectionDetect();
+      } catch (err) {
+        console.log('error', err);
+        expect(err).to.be.an('error');
+      }
+    });*/
     it('should return false', () => {
-      expect(geo.intersectionDetect()).to.equal(false);
+      expect(geo.intersectionDetect([0, 0, 1, 1], [2, 2, 3, 3])).to.be.equal(false);
+    });
+
+    it('should return true', () => {
+      expect(geo.intersectionDetect([0, 0, 0, 1], [-1, -0.5, 1, 0.5])).to.be.equal(true);
     });
   })
 });
