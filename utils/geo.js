f87f30f8b56ff136bb4cba3fe2408fe0e12c34a0
sealionkat <sealionkat@gmail.com>
2017-05-03 21:12:45 +0200

    Added finding intersection point.

--- utils/geo.js
+++ utils/geo.js
@@ -21,14 +21,38 @@ const geo = {
     let s = (-dy1 * (sx1 - sx2) + dx1 * (sy1 - sy2)) / (-dx2 * dy1 + dx1 * dy2);
     let t = ( dx2 * (sy1 - sy2) - dy2 * (sx1 - sx2)) / (-dx2 * dy1 + dx1 * dy2);
 
-    if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
-      return true;
-    }
-
-    return false;
+    return s >= 0 && s <= 1 && t >= 0 && t <= 1;
   },
   distance(p1, p2) {
     return Math.sqrt((p1[0] - p2[0]) * (p1[0] - p2[0]) + (p1[1] - p2[1]) * (p1[1] - p2[1]));
+  },
+  intersectionPoint(seg1, seg2) {
+    if (typeof seg1 === 'undefined' || typeof seg2 === 'undefined') {
+      throw new Error('lack of segment');
+    }
+
+    const sx1 = seg1[0];
+    const sy1 = seg1[1];
+    const ex1 = seg1[2];
+    const ey1 = seg1[3];
+    const sx2 = seg2[0];
+    const sy2 = seg2[1];
+    const ex2 = seg2[2];
+    const ey2 = seg2[3];
+
+    let dx1 = ex1 - sx1;
+    let dy1 = ey1 - sy1;
+    let dx2 = ex2 - sx2;
+    let dy2 = ey2 - sy2;
+
+    let s = (-dy1 * (sx1 - sx2) + dx1 * (sy1 - sy2)) / (-dx2 * dy1 + dx1 * dy2);
+    let t = ( dx2 * (sy1 - sy2) - dy2 * (sx1 - sx2)) / (-dx2 * dy1 + dx1 * dy2);
+
+    if (s >= 0 && s <= 1 && t >= 0 && t <= 1) {
+      return [sx1 + (t * dx1), sy1 + (t * dy1)];
+    }
+
+    return null;
   }
 };
 
--- utils/geo.test.js
+++ utils/geo.test.js
@@ -4,14 +4,6 @@ const geo = require('./geo');
 
 describe('geo utilis', () => {
   describe('intersectionDetection()', () => {
-    /*it('should throw error', () => {
-      try {
-        geo.intersectionDetect();
-      } catch (err) {
-        console.log('error', err);
-        expect(err).to.be.an('error');
-      }
-    });*/
     it('should return false', () => {
       expect(geo.intersectionDetect([0, 0, 1, 1], [2, 2, 3, 3])).to.be.equal(false);
     });
@@ -26,6 +18,16 @@ describe('geo utilis', () => {
       expect(geo.distance([0, 0], [0, 1])).to.be.equal(1);
       expect(geo.distance([0, 0], [1, 0])).to.be.equal(1);
       expect(geo.distance([0, 0], [1, 1])).to.be.within(1.414, 1.415);
+    });
+  });
+
+  describe('intersectionPoint()', () => {
+    it('should return null', () => {
+      expect(geo.intersectionPoint([0, 0, 1, 1], [2, 2, 3, 3])).to.be.equal(null);
+    });
+
+    it('should return array of coordinates', () => {
+      expect(geo.intersectionPoint([0, 0, 2, 2], [0, 2, 2, 0])).to.deep.equal([1, 1]);
     });
   });
 });
