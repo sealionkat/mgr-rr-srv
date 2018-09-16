d4abb08729683c4958f9b16d08205cdca19c811f
sealionkat <sealionkat@users.noreply.github.com>
2018-09-16 17:45:13 +0200

    Added curried normalization.

--- utils/MathExp.js
+++ utils/MathExp.js
@@ -2,6 +2,12 @@ const MathExp = {
   // expecting that max >= x >= min
   normalize(x, max, min) {
     return (x - min) / (max - min);
+  },
+  normalizeGeneral(x, max, min, nmax, nmin) {
+
+  },
+  normalizeCurried(max, min) {
+    return x => (x - min) / (max - min);
   }
 };
 
--- utils/MathExp.test.js
+++ utils/MathExp.test.js
@@ -2,17 +2,17 @@ const expect = require('chai').expect;
 const MathExp = require('./MathExp');
 
 describe('Math expanded', () => {
+  const nm = {
+    max: 100,
+    min: 0
+  };
+
+  const nm2 = {
+    max: 1,
+    min: -1
+  };
+
   describe('normalize()', () => {
-    const nm = {
-      max: 100,
-      min: 0
-    };
-
-    const nm2 = {
-      max: 1,
-      min: -1
-    };
-
     it('should equal 0', () => {
       expect(MathExp.normalize(0, nm.max, nm.min)).to.be.equal(0);
       expect(MathExp.normalize(-1, nm2.max, nm2.min)).to.be.equal(0);
@@ -28,4 +28,24 @@ describe('Math expanded', () => {
       expect(MathExp.normalize(0, nm2.max, nm2.min)).to.be.equal(0.5);
     });
   });
+
+  describe('normalizeCurried()', () => {
+    const nmCurried = MathExp.normalizeCurried(nm.max, nm.min);
+    const nmCurried2 = MathExp.normalizeCurried(nm2.max, nm2.min);
+
+    it('should equal 0', () => {
+      expect(nmCurried(0)).to.be.equal(0);
+      expect(nmCurried2(-1)).to.be.equal(0);
+    });
+
+    it('should equal 1', () => {
+      expect(nmCurried(100)).to.be.equal(1);
+      expect(nmCurried2(1)).to.be.equal(1);
+    });
+
+    it('should equal 0.5', () => {
+      expect(nmCurried(50)).to.be.equal(0.5);
+      expect(nmCurried2(0)).to.be.equal(0.5);
+    });
+  })
 });
